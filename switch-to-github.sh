013a4a7edfefb8b656d3fe5e30b2cfdbde130c69
Леонид Юрьев (Leonid Yuriev) <leo@yuriev.ru>
2026-03-23 00:06:46 +0300

    initial

---
+++ switch-to-github.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git submodule set-url libmdbx https://github.com/Mithril-mine/libmdbx.git
---
+++ switch-to-sourcecraft.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+git submodule set-url libmdbx https://git@git.sourcecraft.dev/dqdkfa/libmdbx.git
